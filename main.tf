d84d6026e7fc6ff12926e25272bd705d0a5ed8e0
Anders Strömberg <coltlaboy@yahoo.se>
2021-05-21 09:40:32 +0200

    update pipeline and main.tf with build version and variables

--- main.tf
+++ main.tf
@@ -11,29 +11,34 @@ terraform {
   }
 }
 
+variable "imagebuild" {
+  type                    = string
+  description             = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "tf_test" {
-  name                = "tfmainrg"
-  location            = "northeurope"
+  name                    = "tfmainrg"
+  location                = "northeurope"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-  name                  = "weatherapi"
-  location              = azurerm_resource_group.tf_test.location
-  resource_group_name   = azurerm_resource_group.tf_test.name
+  name                    = "weatherapi"
+  location                = azurerm_resource_group.tf_test.location
+  resource_group_name     = azurerm_resource_group.tf_test.name
 
-  ip_address_type       = "public"
-  dns_name_label        = "coltlawa"
-  os_type               = "Linux"
+  ip_address_type         = "public"
+  dns_name_label          = "coltlawa"
+  os_type                 = "Linux"
 
   container {
-    name                = "weatherapi"
-    image               = "coltla/weatherapi"
-    cpu                 = "1"
-    memory              = "1"
+    name                  = "weatherapi"
+    image                 = "coltla/weatherapi:${var.imagebuild}"
+    cpu                   = "1"
+    memory                = "1"
 
     ports {
-      port              = 80
-      protocol          = "TCP"
+      port                = 80
+      protocol            = "TCP"
     }
   }
 }
