9df6741b7ddeaeec00b12a6a3aed7fc958b28382
Anders Strömberg <coltlaboy@yahoo.se>
2021-05-21 08:38:06 +0200

    update pipeline file

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfblobrg"
+    storage_account_name  = "tfstoragecoltla"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name                = "tfmainrg"
   location            = "northeurope"
